1a10893fee629ae71ffa949c5568df23876b1d20
noituri <yknomeh@protonmail.com>
2020-03-07 23:30:36 +0100

    web: detecting selected category

--- web/src/utter/pages/commands.cljs
+++ web/src/utter/pages/commands.cljs
@@ -5,14 +5,26 @@
    [utter.components.optionspanel :refer [options-panel]]
    [utter.components.utterlist :refer [utter-list]]))
 
+(def commands
+  [{:kind 1 :name ".help" :description "Shows infromations about commands"}
+   {:kind 1 :name ".prefix" :description "Changes prefix"}
+   {:kind 2 :name ".ban" :description "Bans users from your server"}])
+
+(def categories
+  [{:name "All Commands" :icon "A"}
+   {:name "Main Commands" :icon "M"}
+   {:name "Moderation Commands" :icon "M"}
+   {:name "Ticket Commands" :icon "T"}])
+
 (defn commands-page []
-  (let [title (r/atom "All Commands")]
+  (let [selected-category (r/atom 0)]
     (fn []
       [container {:title "UtterBot - Commands"}
-       [options-panel {:title @title
-                       :options [{:name "A" :selected? true}
-                                 {:name "M" :selected? false}
-                                 {:name "M" :selected? false}
-                                 {:name "T" :selected? false}]}]
-       [utter-list {:entries [{:name ".help" :description "Shows infromations about commands"}
-                              {:name ".prefix" :description "Changes prefix"}]}]])))
+       [options-panel {:title (categories @selected-category)
+                       :options (map-indexed #(hash-map :name (%2 :icon)
+                                                        :selected? (= @selected-category %1))
+                                             categories)}]
+       [utter-list {:entries 
+                    (if (= @selected-category 0)
+                      commands
+                      (filterv #(= (:kind %) @selected-category) commands))}]])))
